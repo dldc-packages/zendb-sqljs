1e907119b371f034e5885fa9d4156a6f7efa7372
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2023-01-05 08:21:37 +0100

    Wip

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,69 @@
-import { IDriver, IDriverDatabase, IDriverStatement } from 'zendb';
-import { SqlJsStatic, Database, Statement } from 'sql.js';
+import * as zen from 'zendb';
+import { Database as SqljsDatabase } from 'sql.js';
 
-export type FileStore = { main: Uint8Array | null; migration: Uint8Array | null };
+export type IDataBase<Schema extends zen.ISchemaAny> = zen.IDatabase<Schema, 'Result'>;
 
-export class Driver implements IDriver<DriverDatabase> {
-  public readonly sqlJsStatic: SqlJsStatic;
-  public readonly files: FileStore;
+export const Database = (() => {
+  return { create };
 
-  constructor(sqlJsStatic: SqlJsStatic, files: FileStore) {
-    this.sqlJsStatic = sqlJsStatic;
-    this.files = files;
-  }
+  function create<Schema extends zen.ISchemaAny>(schema: Schema, db: SqljsDatabase) {
+    return zen.Database.create<Schema, 'Result'>(schema, operationResolver);
 
-  openMain(): DriverDatabase {
-    return new DriverDatabase(new this.sqlJsStatic.Database(this.files.main));
-  }
-
-  openMigration(): DriverDatabase {
-    return new DriverDatabase(new this.sqlJsStatic.Database(this.files.migration));
-  }
-
-  removeMain(): void {
-    this.files.main = null;
-  }
-
-  removeMigration(): void {
-    this.files.migration = null;
-  }
-
-  applyMigration(): void {
-    this.files.main = this.files.migration;
-    this.files.migration = null;
-  }
-}
-
-export class DriverDatabase implements IDriverDatabase<DriverStatement> {
-  public readonly db: Database;
-
-  constructor(db: Database) {
-    this.db = db;
-  }
-
-  prepare(source: string): DriverStatement {
-    return new DriverStatement(this.db, this.db.prepare(source));
-  }
-
-  transaction(fn: () => void): void {
-    console.warn('Transactions are not supported by sql.js');
-    fn();
-  }
-
-  exec(source: string): this {
-    this.db.exec(source);
-    return this;
-  }
-
-  close(): void {
-    this.db.close();
-  }
-
-  getUserVersion(): number {
-    const res = this.db.exec('PRAGMA user_version');
-    return res[0].values[0][0] as number;
-  }
-
-  setUserVersion(version: number): void {
-    this.db.exec(`PRAGMA user_version = ${version}`);
-  }
-}
-
-export class DriverStatement implements IDriverStatement {
-  public readonly statement: Statement;
-  public readonly db: Database;
-
-  constructor(db: Database, statement: Statement) {
-    this.db = db;
-    this.statement = statement;
-  }
-
-  run(): { changes: number } {
-    this.statement.run();
-    const changes = this.db.getRowsModified();
-    return { changes: changes };
-  }
-
-  all(): any[] {
-    const results: Array<Record<string, any>> = [];
-    while (this.statement.step()) {
-      results.push(this.statement.getAsObject());
+    function operationResolver(op: zen.IOperation) {
+      if (op.kind === 'CreateTable') {
+        db.exec(op.sql);
+        return;
+      }
+      if (op.kind === 'Insert') {
+        db.prepare(op.sql).run(...op.params);
+        return op.parse();
+      }
+      if (op.kind === 'Delete') {
+        const stmt = db.prepare(op.sql);
+        if (op.params) {
+          stmt.bind(op.params);
+        }
+        stmt.run();
+        stmt.free();
+        return op.parse({ deleted: db.getRowsModified() });
+      }
+      if (op.kind === 'Update') {
+        const stmt = db.prepare(op.sql);
+        if (op.params) {
+          stmt.bind(op.params);
+        }
+        stmt.run();
+        stmt.free();
+        return op.parse({ updated: db.getRowsModified() });
+      }
+      if (op.kind === 'Query') {
+        const stmt = db.prepare(op.sql);
+        if (op.params) {
+          stmt.bind(op.params);
+        }
+        const results: Array<Record<string, any>> = [];
+        while (stmt.step()) {
+          results.push(stmt.getAsObject());
+        }
+        stmt.reset();
+        stmt.free();
+        return op.parse(results);
+      }
+      if (op.kind === 'ListTables') {
+        const stmt = db.prepare(op.sql);
+        const results: Array<Record<string, any>> = [];
+        while (stmt.step()) {
+          results.push(stmt.getAsObject());
+        }
+        stmt.reset();
+        stmt.free();
+        return op.parse(results);
+      }
+      return expectNever(op);
     }
-    this.statement.reset();
-    return results;
   }
 
-  bind(...params: any[]): this {
-    this.statement.bind(...params);
-    return this;
+  function expectNever(val: never): never {
+    throw new Error(`Unexpected value: ${val}`);
   }
-
-  free() {
-    this.statement.free();
-    return;
-  }
-}
+})();
