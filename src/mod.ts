3b0a803bce61f277ae75435730a0e07954b7a707
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2025-10-07 00:42:22 +0200

    Upgrade zendb to v11

--- src/mod.ts
+++ src/mod.ts
@@ -1,118 +1,136 @@
-import type * as zen from '@dldc/zendb';
-import type { Database as SqljsDatabase } from 'sql.js';
+import type {
+  Driver,
+  TCreateTableOperation,
+  TDeleteOperation,
+  TDropTableOperation,
+  TInsertOperation,
+  TListTablesOperation,
+  TOperation,
+  TOperationResult,
+  TPragmaOperation,
+  TPragmaSetOperation,
+  TQueryOperation,
+  TUpdateOperation,
+} from '@dldc/zendb';
+import type { Database, SqlJsStatic } from 'sql.js';
 
-export interface ISqlJsDatabase {
-  exec<Op extends zen.TOperation>(op: Op): zen.TOperationResult<Op>;
-  execMany<Op extends zen.TOperation>(ops: Op[]): zen.TOperationResult<Op>[];
-  readonly database: SqljsDatabase;
+export interface TSqlJsDriver extends Driver.TDriver<Database> {
+  sqlJsStatic: null | SqlJsStatic;
+  setSqlJsStatic: (sqlJsStatic: SqlJsStatic) => void;
 }
 
-export const SqlJsDatabase = (() => {
-  return { create };
-
-  function create(database: SqljsDatabase) {
-    return {
-      exec,
-      execMany,
-      database,
-    };
-
-    function exec<Op extends zen.TOperation>(op: Op): zen.TOperationResult<Op> {
-      if (op.kind === 'CreateTable') {
-        database.exec(op.sql);
-        return opResult<zen.TCreateTableOperation>(null);
-      }
-      if (op.kind === 'DropTable') {
-        database.exec(op.sql);
-        return opResult<zen.TDropTableOperation>(null);
-      }
-      if (op.kind === 'Insert') {
-        database.exec(op.sql, op.params ? prepareParams(op.params) : null);
-        return opResult<zen.TInsertOperation<any>>(op.parse());
-      }
-      if (op.kind === 'InsertMany') {
-        database.exec(op.sql, op.params ? prepareParams(op.params) : null);
-        return opResult<zen.TInsertOperation<any>>(op.parse());
-      }
-      if (op.kind === 'Delete') {
-        const stmt = database.prepare(op.sql);
-        if (op.params) {
-          stmt.bind(prepareParams(op.params));
-        }
-        stmt.run();
-        stmt.free();
-        return opResult<zen.TDeleteOperation>(op.parse({ deleted: database.getRowsModified() }));
-      }
-      if (op.kind === 'Update') {
-        const stmt = database.prepare(op.sql);
-        if (op.params) {
-          stmt.bind(prepareParams(op.params));
-        }
-        stmt.run();
-        stmt.free();
-        return opResult<zen.TUpdateOperation>(op.parse({ updated: database.getRowsModified() }));
-      }
-      if (op.kind === 'Query') {
-        const stmt = database.prepare(op.sql);
-        if (op.params) {
-          stmt.bind(prepareParams(op.params));
-        }
-        const results: Array<Record<string, any>> = [];
-        while (stmt.step()) {
-          results.push(stmt.getAsObject());
-        }
-        stmt.reset();
-        stmt.free();
-        return opResult<zen.TQueryOperation<any>>(op.parse(results));
-      }
-      if (op.kind === 'ListTables') {
-        const stmt = database.prepare(op.sql);
-        const results: Array<Record<string, any>> = [];
-        while (stmt.step()) {
-          results.push(stmt.getAsObject());
-        }
-        stmt.reset();
-        stmt.free();
-        return opResult<zen.TListTablesOperation>(op.parse(results));
-      }
-      if (op.kind === 'Pragma') {
-        const stmt = database.prepare(op.sql);
-        stmt.step();
-        const result = stmt.getAsObject();
-        stmt.reset();
-        stmt.free();
-        return opResult<zen.TPragmaOperation<any>>(op.parse([result]));
-      }
-      if (op.kind === 'PragmaSet') {
-        database.exec(op.sql);
-        return opResult<zen.TPragmaSetOperation>(null);
-      }
-
-      return expectNever(op);
+export const SqlJsDriver: TSqlJsDriver = {
+  sqlJsStatic: null,
+  setSqlJsStatic: (sqlJsStatic: SqlJsStatic) => {
+    if (SqlJsDriver.sqlJsStatic) {
+      console.warn('sql.js is already set. Overriding the existing instance.');
     }
-
-    function opResult<Op extends zen.TOperation>(res: zen.TOperationResult<Op>): zen.TOperationResult<zen.TOperation> {
-      return res;
+    SqlJsDriver.sqlJsStatic = sqlJsStatic;
+  },
+  createDatabase: () => {
+    if (!SqlJsDriver.sqlJsStatic) {
+      throw new Error('sql.js is not set. Please call `setSqlJsStatic` first.');
     }
+    return new SqlJsDriver.sqlJsStatic.Database();
+  },
+  exec,
+  execMany,
+};
 
-    function execMany<Op extends zen.TOperation>(ops: Op[]): zen.TOperationResult<Op>[] {
-      return ops.map((op) => exec(op));
+function exec<Op extends TOperation>(db: Database, op: Op): TOperationResult<Op> {
+  if (op.kind === 'CreateTable') {
+    db.exec(op.sql);
+    return opResult<TCreateTableOperation>(null);
+  }
+  if (op.kind === 'DropTable') {
+    db.exec(op.sql);
+    return opResult<TDropTableOperation>(null);
+  }
+  if (op.kind === 'Insert') {
+    db.exec(op.sql, op.params ? prepareParams(op.params) : null);
+    return opResult<TInsertOperation<any>>(op.parse());
+  }
+  if (op.kind === 'InsertMany') {
+    db.exec(op.sql, op.params ? prepareParams(op.params) : null);
+    return opResult<TInsertOperation<any>>(op.parse());
+  }
+  if (op.kind === 'Delete') {
+    const stmt = db.prepare(op.sql);
+    if (op.params) {
+      stmt.bind(prepareParams(op.params));
     }
+    stmt.run();
+    stmt.free();
+    return opResult<TDeleteOperation>(op.parse({ deleted: db.getRowsModified() }));
+  }
+  if (op.kind === 'Update') {
+    const stmt = db.prepare(op.sql);
+    if (op.params) {
+      stmt.bind(prepareParams(op.params));
+    }
+    stmt.run();
+    stmt.free();
+    return opResult<TUpdateOperation>(op.parse({ updated: db.getRowsModified() }));
+  }
+  if (op.kind === 'Query') {
+    const stmt = db.prepare(op.sql);
+    if (op.params) {
+      stmt.bind(prepareParams(op.params));
+    }
+    const results: Array<Record<string, any>> = [];
+    while (stmt.step()) {
+      results.push(stmt.getAsObject());
+    }
+    stmt.reset();
+    stmt.free();
+    return opResult<TQueryOperation<any>>(op.parse(results));
+  }
+  if (op.kind === 'ListTables') {
+    const stmt = db.prepare(op.sql);
+    const results: Array<Record<string, any>> = [];
+    while (stmt.step()) {
+      results.push(stmt.getAsObject());
+    }
+    stmt.reset();
+    stmt.free();
+    return opResult<TListTablesOperation>(op.parse(results));
+  }
+  if (op.kind === 'Pragma') {
+    const stmt = db.prepare(op.sql);
+    stmt.step();
+    const result = stmt.getAsObject();
+    stmt.reset();
+    stmt.free();
+    return opResult<TPragmaOperation<any>>(op.parse([result]));
+  }
+  if (op.kind === 'PragmaSet') {
+    db.exec(op.sql);
+    return opResult<TPragmaSetOperation>(null);
   }
 
-  function expectNever(val: never): never {
-    throw new Error(`Unexpected value: ${val as any}`);
-  }
+  return expectNever(op);
+}
 
-  function mapKeys<T extends Record<string, any>>(obj: T, transformKey: (key: string) => string): T {
-    return Object.fromEntries(
-      Object.entries(obj).map(([key, val]) => {
-        return [transformKey(key), val];
-      }),
-    ) as any;
-  }
+function execMany<Op extends TOperation>(db: Database, ops: Op[]): TOperationResult<Op>[] {
+  return ops.map((op) => exec(db, op));
+}
 
-  function prepareParams(params: Record<string, any>): Record<string, any> {
-    return mapKeys(params, (key) => `:${key}`);
-  }
-})();
+function opResult<Op extends TOperation>(res: TOperationResult<Op>): TOperationResult<TOperation> {
+  return res;
+}
+
+function expectNever(val: never): never {
+  throw new Error(`Unexpected value: ${val as any}`);
+}
+
+function mapKeys<T extends Record<string, any>>(obj: T, transformKey: (key: string) => string): T {
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, val]) => {
+      return [transformKey(key), val];
+    }),
+  ) as any;
+}
+
+function prepareParams(params: Record<string, any>): Record<string, any> {
+  return mapKeys(params, (key) => `:${key}`);
+}
